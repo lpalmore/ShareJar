71125c2b99cb858db5289438bec453e727bb3751
Lauren Palmore <ltpalmore@gmail.com>
2016-11-20 22:23:17 -0500

    password reset/change password

--- sharejarsite/urls.py
+++ sharejarsite/urls.py
@@ -23,4 +23,10 @@ urlpatterns = [
     url(r'^login/$', auth_views.login), #login
     url('^', include('django.contrib.auth.urls')), #required for login - login views available
     url(r'^logout/$', auth_views.logout),  #logout
+    url(r'^password_change/', auth_views.password_change),
+    url(r'^password_change/done/$', auth_views.password_change_done),
+    url(r'^password_reset/$', auth_views.password_reset),
+    url(r'^password_reset/done/$', auth_views.password_reset_done),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm),
+    url(r'^reset/done/$', auth_views.password_reset_complete),
 ]
